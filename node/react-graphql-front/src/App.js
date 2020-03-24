6b084f4cdf9050257948426742f09003bd792ab9
Max Puckeridge <max.puckeridge@gmail.com>
2020-03-24 11:43:07 +1100

    Works

--- node/react-graphql-front/src/App.js
+++ node/react-graphql-front/src/App.js
@@ -1,13 +1,22 @@
+import ApolloClient, { gql } from 'apollo-boost';
+import { ApolloProvider } from '@apollo/react-hooks';
 import React, { Component} from "react";
 import {hot} from "react-hot-loader";
 import "./App.css";
+import Books from "./Books";
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+});
 
 class App extends Component{
   render(){
     return(
-      <div className="App">
-        <h1> Hello, World! What's up?</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <div className="App">
+          <Books/>
+        </div>
+      </ApolloProvider>
     );
   }
 }
---
+++ node/react-graphql-front/src/Books.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useQuery } from '@apollo/react-hooks';
+import { gql } from 'apollo-boost';
+
+const BOOKS = gql`
+  {
+    books {
+        title,
+        author
+    }
+  }
+`;
+
+export default function Books() {
+  const { loading, error, data } = useQuery(BOOKS);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.books.map(({ title, author }) => (
+    <div key={title}>
+      <p>
+        {title} by {author}
+      </p>
+    </div>
+  ));
+}
