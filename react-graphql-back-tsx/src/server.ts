3165babc13c078da11e7d4ad59128d3e23a626b9
Max Puckeridge <max.puckeridge@gmail.com>
2020-03-24 16:35:30 +1100

    TS compatible node

---
+++ react-graphql-back-tsx/src/server.ts
@@ -0,0 +1,36 @@
+import { ApolloServer, gql } from 'apollo-server';
+
+const typeDefs = gql`
+  type Book {
+    title: String
+    author: String
+  }
+
+  type Query {
+    books: [Book]
+  }
+`;
+
+const books = [
+  {
+    title: 'Harry Potter and the Chamber of Secrets',
+    author: 'J.K. Rowling',
+  },
+  {
+    title: 'Jurassic Park',
+    author: 'Michael Crichton',
+  },
+];
+
+const resolvers = {
+  Query: {
+    books: () => books,
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => {
+  // tslint:disable-next-line:no-console
+  console.log(`🚀  Server ready at ${url}`);
+});
